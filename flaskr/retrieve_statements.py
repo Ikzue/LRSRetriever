4657ef9d899e2f933d7367ebe805a287ee99917b
Adil <adil-75@live.fr>
2020-06-25 23:32:34 +0200

    Retrieve by ID

--- flaskr/retrieve_statements.py
+++ flaskr/retrieve_statements.py
@@ -20,31 +20,48 @@ def get_date(form_date):
     date = date.replace(hour=0, minute=0, second=0, microsecond=0)
     return date.isoformat() + "Z"
     pass
+
     
 def get_statements(form):
     url = "https://lrsmocah.lip6.fr/api/statements/aggregate"
     auth = "Basic Yzc1NjdmNTZhNGRjOTk2MWU5OTY2NzE5ZDZmZDQ4YzhjZDc4MGY0MzpjYTY4MDQ5ODhmZGU0N2QyZThjYzQyMmUxNzQ3ZmJkMjg0NGEwNjY0"
     headers = {"Authorization": auth}
+
+    #Get statements from hash numbers
     #pipeline_params = [{"$limit": 1}, {"$project": { "statement": 1, "_id": 0 }}]
     pipeline_params = [{"$project": { "statement": 1, "_id": 0 ,"timestamp":1}}]
     date = get_date(form["date"])
     match = {"$match": {
         "statement.actor.name": {"$regex":"{}".format(form['student-hash'])},
-        "statement.verb.id": {"$regex":"{}".format(form['verb-name'])},
-        "statement.object.id": {"$regex":"{}".format(form['activity-name'])},
+        #"statement.verb.id": {"$regex":"{}".format(form['verb-name'])},
+        #"statement.object.id": {"$regex":"{}".format(form['activity-name'])},
     }}
     if date:
         match["$match"]["timestamp"] = {"$gte":{"$dte":date}}
-    
-        
-    #match["$match"]
-    print(match, file=sys.stdout)
     pipeline_params.append(match)
-    #{ "$dateToString": {"date": "2020-06-15T09:57:47.967Z"} }
     params = {"pipeline": json.dumps(pipeline_params)}
-    #params = {"$limit": 1, "$project": {"statement": 1, "_id": 0, "timestamp":1}}
     r = requests.get(url, params=params,headers=headers, verify=False)
     statements = json.loads(r.text)
+    print(len(statements), file=sys.stdout)
+
+    #Get statements from sessions ID
+    sessions = set()
+    for s in statements:
+        sessions.add(s["statement"]["context"]["extensions"]["https://www.lip6.fr/mocah/invalidURI/extensions/session-id"])
+    sessions = list(sessions)
+    
+    pipeline_params = [{"$project": { "statement": 1, "_id": 0 ,"timestamp":1}}]
+    date = get_date(form["date"])
+    match = {"$match": {
+        "statement.context.extensions.https://www&46;lip6&46;fr/mocah/invalidURI/extensions/session-id": {"$in": sessions},
+    }}
+    if date:
+        match["$match"]["timestamp"] = {"$gte":{"$dte":date}}
+    pipeline_params.append(match)
+    params = {"pipeline": json.dumps(pipeline_params)}
+    r = requests.get(url, params=params,headers=headers, verify=False)
+    statements = json.loads(r.text)
+    print(len(statements), file=sys.stdout)
     return statements
 
 @bp.route('/index', methods=('GET', 'POST'))
@@ -52,9 +69,6 @@ def register():
     statements = {}
     nb_statements = None
     if request.method == 'POST':
-        student_hash = request.form['student-hash']
-        verb_name = request.form['verb-name']
-        activity_name = request.form['activity-name']
         statements = get_statements(request.form)
         nb_statements = len(statements)
     return render_template("index.html", statements=statements, nb_statements=nb_statements)
